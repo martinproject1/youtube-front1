02eef3ef38fe0a4b8caf49780e8171187cbd971c
martinproject1 <tigrantashchyan83@email.ru>
2023-12-10 16:28:58 +0400

    we installed configura gulp4 and gulpfile.js

---
+++ build/js/all.js
@@ -0,0 +1,2 @@
+"use strict";Object.defineProperty(exports,"__esModule",{value:!0}),exports.default=void 0;exports.default=function(e){return"Hello dear ".concat(e)};
+//# sourceMappingURL=all.js.map
